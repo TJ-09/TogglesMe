7a95df083059562d22d683f42980fe20a8ffd499
TJ-09 <tim.forman90@gmail.com>
2021-04-18 17:00:05 +0100

    icons!

--- pages/index.js
+++ pages/index.js
@@ -357,6 +357,11 @@ export default function Home() {
         <meta property="og:site_name" content="Toggles Me" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta charSet="UTF-8"></meta>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       <main className="bg-gradient-to-b from-indigo-300 to-purple-400 relative min-h-screen">
